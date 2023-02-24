bcc3e4d7c4a93b77a4ee27e6d92b759ae650c6f5
Lazy-Brat <57150481+Lazy-Brat@users.noreply.github.com>
2023-02-24 21:44:24 +0530

    Add files via upload

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -0,0 +1,52 @@
+package net.javaguides.springboot.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+	
+	private EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService employeeService) {
+		super();
+		this.employeeService = employeeService;
+	}
+	
+	//http://localhost:8080/hello-world
+	
+	//@GetMapping()
+	//public String helloWorld() {
+	//	return "Hello World!";
+	//}
+	
+	//build create employee REST API
+	@PostMapping()
+	public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
+		return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+	}
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
